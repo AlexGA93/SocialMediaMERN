21fa0ed97e48fd161e76915603d9bc851295895d
AlexGA93 <alexgimat@hotmail.com>
2020-07-14 10:12:35 +0200

    Connecting MongoDB to server.js using mongoose

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+//connect to MongoDB with promise
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+//export
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+//connecting server to MongoDB
+const connect = require("./config/db");
+const connectDB = require("./config/db");
+
 const app = express();
+
+//Connect database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log("Server Started on port " + PORT));
