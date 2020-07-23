33b03e2c0442e051a0c1a89945dd6c7e5ebbd9d5
AlexGA93 <alexgimat@hotmail.com>
2020-07-23 09:11:11 +0200

    Like and Unlike Routes UPDATED

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -38,6 +38,62 @@ router.post(
     }
   }
 );
+//@route GET api/posts
+//@desc Get all posts
+//@access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find(req.params.id).sort({ data: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+//@route GET api/posts/:id
+//@desc Get all posts by ID
+//@access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kinf == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route DELETE api/posts/:id
+//@desc Delete a post
+//@access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check user
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    git;
+
+    await post.remove();
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kinf == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 //@route PUT api/posts/like/:id
 //@desc Like a post
 //@access Private
