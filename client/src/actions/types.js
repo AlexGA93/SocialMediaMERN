6db866f889d997c3a2360db74503449b76d98eb8
Alejandro Gimeno <32788331+AlexGA93@users.noreply.github.com>
2020-09-07 15:24:21 +0200

    load user & set auth token (#83)

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,3 +5,7 @@ export const REMOVE_ALERT = "REMOVEALERT";
 //register user
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAILED = "REGISTER_FAILED";
+
+//user loading
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR ";
