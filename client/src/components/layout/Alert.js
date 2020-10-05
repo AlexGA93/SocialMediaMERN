cf5d2b38d7ae0656f98e44e3ce878f61cd50bdff
AlexGA93 <alexgimat@hotmail.com>
2020-10-05 15:48:51 +0200

    PrivateRoute Correction

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -4,10 +4,9 @@ import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 
-const Alert = (
-  { alerts } // ({alerts}) <===> (props) as alerts with mapstatetoprops
-) =>
-  (alerts !== null) & (alerts.length > 0) &&
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts.length > 0 &&
   alerts.map((alert) => (
     <div key={alert.id} className={`alert alert-${alert.alertType}`}>
       {alert.msg}
