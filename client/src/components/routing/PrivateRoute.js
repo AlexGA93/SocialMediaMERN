d233c290a469da993f9d921ed02b4f8ce542fbf6
AlexGA93 <alexgimat@hotmail.com>
2020-10-06 10:03:22 +0200

    Adding notes

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,20 +1,35 @@
 import React from "react";
+
+// We have propTypes so we're going to use 'Redirect' 
 import { Route, Redirect } from "react-router-dom";
+
 import PropTypes from "prop-types";
+
+//to interact with auth state in our reducer
 import { connect } from "react-redux";
 
 const PrivateRoute = ({
+  // taking Component from component passed
   component: Component,
+  //from mapStateToProps
   auth: { isAuthenticated, loading },
+  //we want any other parameter passed in
   ...rest
 }) => (
+  // We want to redirect with Route tag in function of auth's state
   <Route
+    // passing a bunch os stuff here...
     {...rest}
     render={(props) =>
+
       !isAuthenticated && !loading ? (
+
         <Redirect to="/login" />
+
       ) : (
+
         <Component {...props} />
+
       )
     }
   />
@@ -24,6 +39,7 @@ PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired,
 };
 const mapStateToProps = (state) => ({
+  //we want the auth state
   auth: state.auth,
 });
 export default connect(mapStateToProps)(PrivateRoute);
