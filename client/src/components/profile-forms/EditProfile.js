babb89cc02f0c72046ab62a44926af43178fb3da
Alejandro Gimeno <32788331+AlexGA93@users.noreply.github.com>
2020-11-11 09:26:43 +0100

    Addressing Console Warnings (#98)

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -43,7 +43,7 @@ const EditProfile = ({profile:{ profile ,loading}, createProfile, getCurrentProf
           youtube : loading || !profile.social ? '' : profile.social.youtube,
           instagram : loading || !profile.social ? '' : profile.social.instagram,
       });
-  }, [loading])
+  }, [loading, getCurrentProfile])
 
   //action to interact with the front form's data
   const {
