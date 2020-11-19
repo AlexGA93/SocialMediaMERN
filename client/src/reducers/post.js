204f369bcd83b90c51668763c255c2afd1f121b8
Alejandro Gimeno <32788331+AlexGA93@users.noreply.github.com>
2020-11-19 11:44:26 +0100

    Deleting Post Func (#113)

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,7 +1,9 @@
+
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST
 } from '../actions/types';
 
 
@@ -21,6 +23,14 @@ export default function(state= initialState, action) {
             return {
                 ...state,
                 posts: payload,
+                loading: false
+            }
+        case DELETE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(
+                    post => post._id !== payload
+                ),
                 loading: false
             }
         case POST_ERROR:
